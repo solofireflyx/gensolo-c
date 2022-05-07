eddd0cf8da73a57ec87295b4bf65465f981bd6d8
OFFVENOM_me <78276510+badboyyy098@users.noreply.github.com>
2022-05-07 21:30:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+name = input('What is your username ID?\n')
+print('ID IS, %s.' % name)
+name = input('data adding... TOKEN \n')
+print('token IS, %s.' % name)
+
+for i in range(100000):
+  print('code gen solo/c :', i)
+
+for i in range(100000):
+  print('code get solo/c :', i)
+
+for i in range(100000):
+  print('code input solo/c :', i)
+
+for i in range(100000):
+  print('code adding... solo/c :', i)
+
+for i in range(100000):
+  print('adding... solo/c :', i)
