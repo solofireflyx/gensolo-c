358b5667e483657131ca274a5f55cb55bc3691b1
solo.fireflyx <78276510+solofireflyx@users.noreply.github.com>
2022-05-14 06:53:39 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
-name = input('What is your username ID?\n')
-print('ID IS, %s.' % name)
-name = input('data adding... TOKEN \n')
-print('token IS, %s.' % name)
-
-for i in range(100000):
+for i in range(100):
   print('code gen solo/c :', i)
 
-for i in range(100000):
+for i in range(100):
   print('code get solo/c :', i)
 
-for i in range(100000):
+for i in range(100):
   print('code input solo/c :', i)
 
-for i in range(100000):
+for i in range(100):
   print('code adding... solo/c :', i)
 
-for i in range(100000):
+for i in range(100):
   print('adding... solo/c :', i)
+
+for i in range(1):
+  print('your code :', i)
+
+for i in range(2):
+  print('your code :', i)
+
+for i in range(3):
+  print('your code :', i)
+
+for i in range(4):
+  print('your code :', i)
+
+for i in range(100):
+  print('your code :', i)
+  
